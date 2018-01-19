0466604a453a7d84cdc5f34d11e35529c9b730f1
amerelo <amerelo@student.42.fr>
2018-01-19 07:13:51 +0100

    correct way to test array of options

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 
 fn main()
 {
-	let re = Regex::new(r"(\d+)\s*\*\s*(\D{1})\^(\d+)").unwrap();
+	let re = Regex::new(r"(\d+)\s*\*\s*(\D{1})\^(\d+)\s*(\+|\-|=)?\s*").unwrap();
 
 	let test =  "5854 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
 	// let test =  "21312 hola roro";
@@ -12,6 +12,26 @@ fn main()
 	println!("test {}", re);
 	println!("length {}", re.captures_len());
 	for cap in re.captures_iter(test) {
-		println!("f1: {}, f2: {} f3: {}", &cap[1], &cap[2], &cap[3]);
+		// println!("New line ");
+		// for elem in cap.iter() {
+		// 	match elem 
+		// 	{
+		// 		Some(x) => {
+		// 			println!("ok {:?}", x);
+		// 			// println!("f1: {}, f2: {} f3: {},f4: {}", &cap[1], &cap[2], &cap[3], x);
+		// 		}
+		// 		None => {
+		// 			println!("Error");
+		// 			// println!("f1: {}, f2: {} f3: {}", &cap[1], &cap[2], &cap[3]);
+		// 		}
+		// 	}
+		// }
+
+		if let Some(_m) = cap.get(4) {
+			println!("f1: {}, f2: {}, f3: {}, f4: {}", &cap[1], &cap[2], &cap[3], &cap[4]);
+		}
+		else {
+			println!("f1: {}, f2: {} f3: {}", &cap[1], &cap[2], &cap[3]);
+		}
 	}
 }
