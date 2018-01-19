15f846a7e6e1bf64b40185b376cc2f0eb63d3bbc
amerelo <amerelo@student.42.fr>
2018-01-19 05:51:18 +0100

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,17 @@
-extern crate rand;
 extern crate regex;
 
-use std::io;
 use regex::Regex;
-// use std::cmp::Ordering;
-// use rand::Rng;
-//  3 * X^0 + 1 * X^1 = 0
-// -1 * X^0 - 2 * X^1 = 1 * X^0 + 2 * X^1
 
-// struct Sec {
-// 	num: i32,
-// 	pow: i32,
-// }
+fn main()
+{
+	let re = Regex::new(r"(\d+)\s*\*\s*(\D{1})\^(\d+)").unwrap();
 
-fn main() {
-
-	println!("Entrez une equation: ");
-
-	let mut equa = String::new();
-	io::stdin().read_line(&mut equa).expect("Failed to read line");
-
-	let re = Regex::new(r"\s+").unwrap();
-	let result = re.replace_all(equa.as_str().trim(), " ");
-	let results = result.split(" ");
-
-	for letter in results{
-		println!("|{}|", letter);
-		let num = letter.parse::<i32>();
-	    match num {
-	        Ok(val) => println!("Yes, it is a number ({})", val),
-	        Err(why) => println!("Doesn't look like a number ({})", why),
-	    }
+	let test =  "5854 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
+	// let test =  "21312 hola roro";
+	println!("length {}", re.captures_len());
+	println!("test {}", re);
+	println!("length {}", re.captures_len());
+	for cap in re.captures_iter(test) {
+		println!("f1: {}, f2: {} f3: {}", &cap[1], &cap[2], &cap[3]);
 	}
-	println!("|{}|", result);
-
 }
-
-
-// loop {
-// 	println!("Please input your guess.");
-//
-// 	let guess: u32 = match guess.trim().parse() {
-// 		Ok(num) => num,
-// 		Err(_) => {
-// 			println!("Please type a nbr.");
-// 			continue;
-// 		}
-// 	};
-//
-// 	println!("You guessed: {}", guess);
-//
-// 	match guess.cmp(&secret_number)
-// 	{
-// 		Ordering::Less    => println!("Too small!"),
-// 		Ordering::Greater => println!("Too big!"),
-// 		Ordering::Equal   => {
-// 			println!("You win!");
-// 			break;
-// 		}
-// 	}
-// }
